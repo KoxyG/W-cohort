0872f33fb5a87a845332d63947b6ec81fd8855f3
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-10-12 20:50:06 +0100

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,102 @@
-import './App.css'
-import Button from './component/Button'
+import "./App.css";
+import Button from "./component/Button";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
- 
-   const background = "/screw.jpg";
-  
+  const background = "/screw.jpg";
 
   return (
     <>
-     <div>
-      <div style={{ backgroundImage: `url(${background})` }} className='z-10 bg-cover bg-center h-screen flex items-center justify-center'>
-        <div>
-          <div className='grid text-[#c4b3da] '>
-            <h1 className='text-[10px] md:pl-[20px] tracking-wide md:text-[17px] pb-2 md:pb-6 leading-3'>BRITTEN</h1>
-            <h1 className='text-[30px] md:pl-[15px]  md:text-[45px]  py-[10px] leading-[5px]'>THE</h1>
-          </div>
+      <div>
+        <AnimatePresence >
+        <motion.div
+        key="zoom"
+        initial={{ scale: 1 }}
+        animate={{ scale: [1, 1.2, 1] }}
+        exit={{ scale: 0.5 }}
+        transition={{
+          repeat: Infinity,
           
-          <div className='flex text-[#c4b3da] space-x-2 '>
-            <h1 className='text-[30px] md:text-[150px]  md:pt-[50px] leading-9'>TURN</h1>
-            <div className='grid text-[10px]  md:text-[35px]'>
-              <h1 className='md:pt-[30px]'>OF</h1>
-              <h1  className=''>THE</h1>
-            </div> 
-            <h1 className='text-[30px] md:text-[150px]   md:pt-[50px] leading-9'>SCREW</h1>
+          duration: 20,
+          ease: 'easeInOut',
+        }}
+          style={{ backgroundImage: `url(${background})` }}
+          className="z-10 bg-cover bg-center h-screen flex items-center justify-center"
+        >
+          <div>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                ease: "linear",
+                duration: 1,
+                x: { duration: 1 },
+              }}
+              className="grid text-[#c4b3da] "
+            >
+              <h1 className="text-[10px] md:pl-[20px] tracking-wide md:text-[17px] pb-2 md:pb-6 leading-3">
+                BRITTEN
+              </h1>
+              <h1 className="text-[30px] md:pl-[15px]  md:text-[45px]  py-[10px] leading-[5px]">
+                THE
+              </h1>
+            </motion.div>
+
+            <div className="flex text-[#c4b3da] space-x-2 ">
+              <motion.h1 initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }} transition={{
+              ease: "linear",
+              duration: 1,
+              y: { duration: 1 }
+            }}
+               className="text-[30px] md:text-[150px]  md:pt-[50px] leading-9">
+                TURN
+              </motion.h1>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  ease: "linear",
+                  duration: 1,
+                  x: { duration: 1 },
+                }}
+                className="grid text-[10px]  md:text-[35px]"
+              >
+                <h1 className="md:pt-[30px]">OF</h1>
+                <h1 className="">THE</h1>
+              </motion.div>
+              <motion.h1 initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }} transition={{
+              ease: "linear",
+              duration: 1,
+              y: { duration: 1 }
+            }}
+              className="text-[30px] md:text-[150px]   md:pt-[50px] leading-9">
+                SCREW
+              </motion.h1>
+            </div>
+
+            <div className="text-[#c4b3da] text-[4px] md:text-[15px] md:leading-9 flex justify-end">
+              <motion.p
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  ease: "linear",
+                  duration: 1,
+                  x: { duration: 1 },
+                }}
+              >
+                An immersive trailer for Opera North production
+              </motion.p>
+            </div>
+
+            <Button />
           </div>
-
-          <div className='text-[#c4b3da] text-[4px] md:text-[15px] md:leading-9 flex justify-end'>
-            <p>An immersive trailer for Opera North production</p>
-          </div>
-
-          <Button />
-        </div>
-
-        
-        
+        </motion.div>
+        </AnimatePresence>
       </div>
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,21 +1,57 @@
+import { motion } from "framer-motion";
+
 const Button = () => {
   return (
     <div className="pt-[50px] grid place-content-center">
-      <p className="text-white  text-[4px]  md:text-[15px] pb-3">
+      <motion.p
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          ease: "linear",
+          duration: 1,
+          x: { duration: 1 },
+        }}
+        className="text-white  text-[4px]  md:text-[15px] pb-3"
+      >
         Please choose the quality according to your device:
-      </p>
+      </motion.p>
       <div className="flex text-white  space-x-6">
         <div>
-          <button className="border ButtonX py-2 px-3 md:py-2 md:px-5 ">Low</button>
-          <p className="text-[3px] md:text-[5px]  text-transparent hover:text-white">Compressed mp3 audio <br /> 30mb</p>
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{ scale: 1.1 }}
+            className="border ButtonX py-2 px-3 md:py-2 md:px-5 "
+          >
+            Low
+          </motion.button>
+          <p className="text-[3px] md:text-[5px]  text-transparent hover:text-white">
+            Compressed mp3 audio <br /> 30mb
+          </p>
         </div>
         <div>
-          <button className="border ButtonX py-2 px-3 md:py-2 md:px-5  ">Medium</button>
-        <p className="text-[3px] md:text-[5px] text-transparent hover:text-white">Standard mp3 audio 30mb</p>
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{ scale: 1.1 }}
+            className="border ButtonX py-2 px-3 md:py-2 md:px-5  "
+          >
+            Medium
+          </motion.button>
+          <p className="text-[3px] md:text-[5px] text-transparent hover:text-white">
+            Standard mp3 audio 30mb
+          </p>
         </div>
         <div className="">
-          <button className="border ButtonX py-2 px-3 md:py-2 md:px-5  ">High</button>
-            <p className="text-[3px] md:text-[5px] pt-[5px] text-transparent hover:text-white">High quality mp3 audio <br />30mb</p>
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{ scale: 1.1 }}
+            className="border ButtonX py-2 px-3 md:py-2 md:px-5  "
+          >
+            High
+          </motion.button>
+          <p className="text-[3px] md:text-[5px] pt-[5px] text-transparent hover:text-white">
+            High quality mp3 audio <br />
+            30mb
+          </p>
         </div>
       </div>
     </div>
